309a1e644b68be5c5d4f610202ae9d6c6452d804
Juan Antonio <juanjimpad@gmail.com>
2026-05-24 22:23:04 +0200

    Fix facturas panel: open modal instead of navigating to coche view

--- js/render.js
+++ js/render.js
@@ -385,7 +385,7 @@ function facturaItem({ clienteKey, cocheKey, facturaKey, factura, cliente, coche
   const avatarClass = ESTADO_AVATAR_CLASS[factura.estado] || '';
   const metodoPagoIcon = METODO_PAGO_ICON[factura.metodoPago] || '';
   return `
-    <li class="item-row factura-item" data-action="select-coche" data-cliente-key="${clienteKey}" data-key="${cocheKey}">
+    <li class="item-row factura-item" data-action="edit-factura" data-cliente-key="${clienteKey}" data-coche-key="${cocheKey}" data-key="${facturaKey}">
       <div class="item-avatar ${avatarClass}">
         <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/></svg>
       </div>
